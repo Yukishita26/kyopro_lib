49262038fbcd9cf7ce2c429f7099febb44aea2b9
Yukishita <sakkii51@live.com>
2021-05-03 01:27:33 +0900

    add: Python diameter of tree

--- python/lib.py
+++ python/lib.py
@@ -18,3 +18,30 @@ class UnionFindTree:
             self._tree[rb] = ra
             if self._rank[ra] == self._rank[rb]:
                 self._rank[ra] += 1
+
+from collections import deque
+# graph(隣接リスト), 起点node -> deepest node, depth
+def bfs_depth(graph, start: int): 
+    dist = [-1] * (len(graph))
+    dist[start] = 0
+    deepest_node = start
+    depth = 0
+    q = deque()
+    q.append(start)
+    while q:
+        top = q.popleft()
+        for next_node in graph[top]:
+            if dist[next_node] != -1:
+                continue
+            dist[next_node] = dist[top] + 1
+            deepest_node = next_node
+            depth = dist[next_node]
+            q.append(next_node)
+    # return dist
+    return deepest_node, depth
+
+# graph(隣接グラフ) -> 木の直径
+def tree_diameter(graph):
+    nodel, _ = bfs_depth(graph, 1)
+    _node, r = bfs_depth(graph, nodel)
+    return r
--- python/test_lib.py
+++ python/test_lib.py
@@ -18,5 +18,70 @@ class TestUFT(unittest.TestCase):
         self.assertEqual(uft.root(1), uft.root(3))
         self.assertNotEqual(uft.root(0), uft.root(3))
 
+class TestTreeDiamiter(unittest.TestCase):
+    def solveStr(self, s):
+        s = s.split("\n")
+        N = int(s[0].strip())
+        d = [[] for _ in range(N+1)]
+        for i in range(N-1):
+            a, b = map(int, s[i+1].strip().split())
+            d[a].append(b)
+            d[b].append(a)
+        return lib.tree_diameter(d)
+        
+    # テストケースは https://atcoder.jp/contests/typical90/tasks/typical90_c より
+    def testTreeDiameter1(self):
+        self.assertEqual(self.solveStr("""5
+1 2
+2 3
+3 4
+3 5
+"""), 3)
+    def testTreeDiameter2(self):
+        self.assertEqual(self.solveStr("""10
+1 2
+1 3
+2 4
+4 5
+4 6
+3 7
+7 8
+8 9
+8 10
+"""), 7)
+    def testTreeDiameter3(self):
+        self.assertEqual(self.solveStr("""31
+1 2
+1 3
+2 4
+2 5
+3 6
+3 7
+4 8
+4 9
+5 10
+5 11
+6 12
+6 13
+7 14
+7 15
+8 16
+8 17
+9 18
+9 19
+10 20
+10 21
+11 22
+11 23
+12 24
+12 25
+13 26
+13 27
+14 28
+14 29
+15 30
+15 31
+"""), 8)
+
 if __name__ == '__main__':
     unittest.main()
